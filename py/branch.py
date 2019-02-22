2008e69669bc0327f0ea2538ba4e497bfedf0cd3
nkweeks <nkweeks@hawaii.edu>
2019-02-22 13:59:43 -1000

    Updated to include the directory structure

---
+++ py/branch.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.7
+"""
+branch.py
+This program performs branching in the form of if statements.
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ run.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3.7
+
+'''
+This program runs and gathers benchmark data about programs written in different languages
+Usage: ./run.py [language] [function]
+Examples:
+    ./run.py -py -calc
+    ./run.py -c -all
+'''
+__author__ = 'Nathaniel Weeks'
+__version__ = '0.0.1'
+
+def run_pl():
+    pass
+
+def run_py():
+    pass
+
+def run_jv():
+    pass
+
+def run_c1():
+    pass
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
