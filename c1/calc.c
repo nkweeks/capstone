977fc43270dac1322afa298fad8c9a07fbb8f2d6
Nathaniel Weeks <nkweeks@hawaii.edu>
2019-03-01 21:27:04 -0800

    Added .gitignore

--- c1/calc.c
+++ c1/calc.c
@@ -3,38 +3,42 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define NUM_OF_LOOPS 10
+#define NUM_OF_LOOPS 100
 
 // Function prototypes
 static long get_nanos(void);
-long pow(long x, long y);
+int mypow(int x, int y);
 
 int main(void)
 {
     long start = get_nanos();
     for (int i = 1; i <= NUM_OF_LOOPS; i++) {
-        // Exponent
-        i = pow(i, i);
+        // Save the counter to a variable that can be used
+        int x = i;
+
+        // Raise the counter to the power of itself
+        x = mypow(i, i);
+        printf("%d ** %d = %d\n", i, i, x); 
         // Multiply
-        i = i * i;
+        x = x * x;
         // Divide
-        i = i / i;
+        x = x / x;
         // Add
-        i = i + i;
+        x = x + x;
         // Subtract
-        i = i - i;
+        x = x - x;
     }
     long end = get_nanos();
     printf("%Lf\n", (((long double)end) - start) / 1000000000);
 }
 
 
-long pow(long x, long y) 
+int mypow(int x, int y) 
 {
     if (y <= 1) {
         return x;
     } else {
-        return pow(x, y-1) * pow(x, y-2);
+        return x * mypow(x, y-1);
     }
 }
 
--- jv/calc.java
+++ jv/calc.java
@@ -2,8 +2,24 @@
 
 
 public class calc {
+    public static final int NUM_OF_LOOPS = 100000;
 
     public static void main(String[] args) {
-        int j;
+        long start = System.nanoTime();
+        for(int i=0; i<NUM_OF_LOOPS; i++){
+            // Save the counter to a variable that can be used
+            int x = i;
+
+            // Raise the counter to the power of itself
+            x = pow(i, i);
+
+        }
+
+        // Calculate the time taken to run
+        long end = System.nanoTime();
+        double elapsedTime = (double)(end - start) / 1000000000; // nanoseconds to seconds
+
+        // Print out the elapsed time
+        System.out.printf("%f\n", elapsedTime);
     }
 }
