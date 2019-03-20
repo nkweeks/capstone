099a548e1215856438bd67b11291e313cf2d5185
Nathaniel Weeks <nkweeks@hawaii.edu>
2019-03-20 11:57:06 -1000

    Simplified the data analysis script by removing unnecessary function.

--- data/process_data.py
+++ data/process_data.py
@@ -9,20 +9,21 @@ import matplotlib.pyplot as plt
 from itertools import product
 
 
-def proc():
-
+def main():
     df = pd.read_csv('data.csv', names=['lang', 'func', 'time'])
     langs = ['C', 'Java', 'Python', 'Perl']
     funcs = ['Branch', 'Calc', 'Loop']
     prev = ''
     which_func = 0
     print(funcs[which_func])
-    print(f'{"Language":14}{"Time":>10}{"Std Deviation":>18}')
+    print(f'{"Language":14}{"Time":>10}    {"Std Deviation":^14}  {"High":^10}{"Low":^10}')
     for i, (func, lang) in enumerate(product(funcs, langs)):
         lang_func_df = df[(df.lang == lang) & (df.func==func)]
         m = round(lang_func_df['time'].mean(), 6)
         s = round(lang_func_df['time'].std(), 6)
-        print(f'{lang:14}{m:>10.6}{s:>18.6}')
+        high = round(lang_func_df['time'].max(), 6)
+        low = round(lang_func_df['time'].min(), 6)
+        print(f'{lang:14}{m:>10.6}    {s:<14.6}  {high:<10.6}{low:<10.6}')
         plt.plot(range(1,101), lang_func_df['time'], label=lang)
         if (i+1) % 4 == 0:
             prev = func
@@ -32,18 +33,14 @@ def proc():
             plt.legend(loc='upper right')
             plt.xlim(0,100)
             plt.ylim(0)
-            plt.show()
+            # plt.show()
             try:
                 which_func += 1
 
                 print(f'\n\n{funcs[which_func]}')
-                print(f'{"Language":14}{"Time":>10}{"Std Deviation":>18}')
+                print(f'{"Language":14}{"Time":>10}    {"Std Deviation":^14}  {"High":^10}{"Low":^10}')
             except:
                 pass
-
-
-def main():
-    proc()
 
 
 if __name__ == '__main__':
